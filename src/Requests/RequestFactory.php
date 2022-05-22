0389d51b55b2c50a21bc6633d2678980a1b39dde
seben <oltmann.sebastian@gmail.com>
2022-05-22 20:52:15 +0200

    Add request facotry

---
+++ src/Requests/RequestFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiteHttpClient\Requests;
+
+use LiteHttpClient\Clients\ClientInterface;
+use LiteHttpClient\Exceptions\InvalidRequestTypeException;
+use LiteHttpClient\Requests\Types\{DeleteRequest, GetRequest, PatchRequest, PostRequest, PutRequest};
+use Psr\Http\Message\RequestFactoryInterface as PSRRequestFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+class RequestFactory implements RequestFactoryInterface
+{
+    /**
+     * @throws InvalidRequestTypeException
+     */
+    public static function make(
+        string $method,
+        UriInterface $uri,
+        PSRRequestFactoryInterface $factory,
+        ClientInterface $client
+    ): RequestInterface {
+        switch ($method) {
+            case HttpRequestType::POST:
+                return new PostRequest($uri, $factory, $client);
+            case HttpRequestType::GET:
+                return new GetRequest($uri, $factory, $client);
+            case HttpRequestType::PUT:
+                return new PutRequest($uri, $factory, $client);
+            case HttpRequestType::PATCH:
+                return new PatchRequest($uri, $factory, $client);
+            case HttpRequestType::DELETE:
+                return new DeleteRequest($uri, $factory, $client);
+            default:
+                throw new InvalidRequestTypeException('Invalid method type.');
+        }
+    }
+}
---
+++ src/Requests/RequestFactoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiteHttpClient\Requests;
+
+use LiteHttpClient\Clients\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface as PSRRequestFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+interface RequestFactoryInterface
+{
+    public static function make(
+        string $method,
+        UriInterface $uri,
+        PSRRequestFactoryInterface $factory,
+        ClientInterface $client
+    ): RequestInterface;
+}
