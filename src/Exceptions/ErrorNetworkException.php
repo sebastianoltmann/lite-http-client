4d9494a389b9df626bb601e7cc8d1482ed694cd8
seben <oltmann.sebastian@gmail.com>
2022-05-22 20:44:27 +0200

    Add exceptions

---
+++ src/Exceptions/ErrorNetworkException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiteHttpClient\Exceptions;
+
+use Exception;
+
+final class ErrorNetworkException extends Exception
+{
+
+}
---
+++ src/Exceptions/InvalidRequestTypeException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiteHttpClient\Exceptions;
+
+use Exception;
+
+final class InvalidRequestTypeException extends Exception
+{
+
+}
